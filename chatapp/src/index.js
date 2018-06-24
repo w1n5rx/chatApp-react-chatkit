e1123cd841e8196d76ee8d9333d21443913ac37f
Vinod Singh Ramalwan <vinod.ramalwan@stud.tu-darmstadt.de>
2018-06-25 01:05:58 +0200

    Components added

--- chatapp/src/index.js
+++ chatapp/src/index.js
@@ -1,8 +1,122 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/19b7716f-8c27-464f-9bd4-a381038f5dcc/token"
+const instanceLocator = "v1:us1:19b7716f-8c27-464f-9bd4-a381038f5dcc"
+const roomId = Math.floor(Math.random() * 20);
+const username = 'test-user'
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            messages: []
+        }
+        this.sendMessage = this.sendMessage.bind(this)
+    } 
+    
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: instanceLocator,
+            userId: 'test-user',
+            tokenProvider: new Chatkit.TokenProvider({
+                url: testToken
+            })
+        })
+        
+        chatManager.connect()
+        .then(currentUser => {
+            this.currentUser = currentUser
+            this.currentUser.subscribeToRoom({
+            roomId: roomId,
+            hooks: {
+                onNewMessage: message => {
+
+                    this.setState({
+                        messages: [...this.state.messages, message]
+                    })
+                }
+            }
+        })
+      })
+    }
+    
+    sendMessage(text) {
+        this.currentUser.sendMessage({
+            text,
+            roomId: roomId
+        })
+    }
+    
+    render() {
+        return (
+            <div className="app">
+              <Title />
+              <MessageList 
+                  roomId={this.state.roomId}
+                  messages={this.state.messages} />
+              <SendMessageForm
+                  sendMessage={this.sendMessage} />
+            </div>
+        );
+    }
+}
+
+class MessageList extends React.Component {
+    render() {
+        return (
+            <ul className="message-list">
+                {this.props.messages.map((message, index) => {
+                    return (
+                      <li  key={message.id} className="message">
+                        <div>{message.senderId}</div>
+                        <div>{message.text}</div>
+                      </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+class SendMessageForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault()
+        this.props.sendMessage(this.state.message)
+        this.setState({
+            message: ''
+        })
+    }
+    
+    render() {
+        return (
+            <form
+                onSubmit={this.handleSubmit}
+                className="send-message-form">
+                <input
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Press Enter to Send"
+                    type="text" />
+            </form>
+        )
+    }
+}
+
+function Title() {
+  return <p className="title">Chat App</p>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
